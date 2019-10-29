0c79d4e89b60a31d7e2a71392b2cbab207c6ec01
TxHammer <txhammer68@users.noreply.github.com>
2019-10-29 15:59:45 -0500

    Add files via upload

--- weather/index.js
+++ weather/index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+var req = require('request');
 const api = require('./api.json');
 const axios = require('axios');
 const fs = require('fs')
+const weatherIcons = require('./icons.json');
 
 const getWeather = async location => {
 	const city = location || 'La Porte, TX USA';
@@ -52,33 +54,90 @@ function printWeather(weather) {
 
 	// console.log(message);
     // console.log(weather.weather[0].main,Math.round(weather.main.temp));
-    // console.log(weather.weather[0].main);
-    // console.log(weather);
-    
+    // console.log(weather.weather[0].description);
+    // console.log(weather.weather[0].icon);
+    // console.log(weather.weather[0].id);
+    function weatherIcon () {
+        var iconurl=``
+        if (weather.weather[0].icon === '01d') {
+        iconurl= `../icons/01d.png`} else if (weather.weather[0].icon === '01n') {
+            iconurl=`../icons/01n.png`}
+            else if  (weather.weather[0].icon === '02d') {
+            iconurl=`../icons/02d.png`}
+        else if  (weather.weather[0].icon === '02n') {
+            iconurl=`../icons/02n.png`}
+        else if  (weather.weather[0].icon === '03d') {
+            iconurl=`../icons/03d.png`}
+        else if  (weather.weather[0].icon === '03n') {
+            iconurl=`../icons/03n.png`}
+            else if  (weather.weather[0].icon === '04n') {
+            iconurl=`../icons/04n.png`}
+            else if  (weather.weather[0].icon === '04d') {
+            iconurl=`../icons/04d.png`}
+            else if  (weather.weather[0].icon === '09n') {
+            iconurl=`../icons/09n.png`}
+            else if  (weather.weather[0].icon === '09d') {
+            iconurl=`../icons/09d.png`}
+            else if  (weather.weather[0].icon === '10n') {
+            iconurl=`../icons/10n.png`}
+            else if  (weather.weather[0].icon === '10d') {
+            iconurl=`../icons/10d.png`}
+            else if  (weather.weather[0].icon === '11n') {
+            iconurl=`../icons/11n.png`}
+            else if  (weather.weather[0].icon === '11d') {
+            iconurl=`../icons/11d.png`}
+            else if  (weather.weather[0].icon === '13n') {
+            iconurl=`../icons/13n.png`}
+            else if  (weather.weather[0].icon === '13d') {
+            iconurl=`../icons/13d.png`}
+            else if  (weather.weather[0].icon === '50n') {
+            iconurl=`../icons/50n.png`}
+            else if  (weather.weather[0].icon === '50d') {
+            iconurl=`../icons/50d.png`}
+            else if  (weather.weather[0].icon === 'na') {
+            iconurl=`../icons/na.png`}
+            return iconurl;
+        }
+       
     let t1 = `var temp  = \x22 `
-                let t2 = `°\x22`
-                let t3 = weather.weather[0].main
+                let t2 = `° `
+                let t3 = weather.weather[0].description
                 let t4 = Math.round(weather.main.temp)
-                let t5 = ` - `
-                // console.log(t3);
-    fs.writeFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t1, function (err) {
+                let t5 = ` -  `
+                let t6 = `\nvar icon =\x22`
+                let t7 = weatherIcon ();
+                let t8 = `\x22`
+                
+fs.writeFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t1, function (err) {
   if (err) throw err;
 });
-    fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t3, function (err) {
-  if (err) throw err;
-});      
-    fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t5, function (err) {
-  if (err) throw err;
-});    
-    
     fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t4, function (err) {
   if (err) throw err;
-});
-                
-                fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t2,
+});      
+    fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t2,
                 function (err) {
   if (err) throw err;
 });
+    fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t5, function (err) {
+  if (err) throw err;
+});    
+    fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t3, function (err) {
+  if (err) throw err;
+});
+fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t8,
+                function (err) {
+  if (err) throw err;
+});
+        fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t6, function (err) {
+  if (err) throw err;
+});    
+        fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t7, function (err) {
+  if (err) throw err;
+});    
+        fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/temp.js`,t8, function (err) {
+  if (err) throw err;
+});    
+                
 }
 
 function printError(error) {
