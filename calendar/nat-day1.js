12513d9891cf62a4c5e7126a827af685cedc7fb5
TxHammer <txhammer68@users.noreply.github.com>
2019-10-29 15:58:45 -0500

    Add files via upload

--- calendar/nat-day1.js
+++ calendar/nat-day1.js
@@ -7,10 +7,13 @@ var m = dt.getMonth()+1;
 var day = dt.getDate();
 const today = `${m}${day}`
 // const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-ical.fromURL('https://calendar.google.com/calendar/ical/9u8jqp3hlt6pe675gie6lf1d9o%40group.calendar.google.com/public/basic.ics', {}, function (err, data) {
+// ical.fromURL('https://calendar.google.com/calendar/ical/9u8jqp3hlt6pe675gie6lf1d9o%40group.calendar.google.com/public/basic.ics', {}, function (err, data) {
+    // ical.parseFile('/run/media/hammer/Data/projects/natday.ics', {}, function (err, data) {
+    var data = ical.parseFile('/run/media/hammer/Data/projects/myical.ics');
 	for (let d  in data) {
 		if (data.hasOwnProperty(d)) {
 			var ev = data[d];
+            // console.log(ev.summary);
 			if (data[d].type == 'VEVENT') {
             if (`${data[d].start.getMonth()+1}${data[d].start.getDate()}` == today) {    
                 // console.log(ev.summary)
@@ -18,25 +21,37 @@ ical.fromURL('https://calendar.google.com/calendar/ical/9u8jqp3hlt6pe675gie6lf1d
                 let t1 = `var today  = \x22`
                 let t2 = `\x22`
                 
-                fs.writeFile(`/run/media/hammer/Data/projects/natday.js`,t1, function (err) {
+                fs.writeFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/natday.js`,t1, function (err) {
   if (err) throw err;
 });
-                fs.appendFile(`/run/media/hammer/Data/projects/natday.js`,ev.summary, function (err) {
+                fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/natday.js`,ev.summary, function (err) {
   if (err) throw err;
 });
                 
-                fs.appendFile(`/run/media/hammer/Data/projects/natday.js`,t2,
+                fs.appendFileSync(`/home/hammer/.local/share/plasma/look-and-feel/DigiTech/contents/code/natday.js`,t2,
                 function (err) {
   if (err) throw err;
 });
+                
                 
                 return ev.summary;
                 // document.writeln(today,ev.summary);
                 // console.log(today)
                 // console.log(`${data[d].start.getMonth()+1}/${data[d].start.getDate()} - ${data[d].summary}`);
+           
+                
             }
+            
+                
             }
-		}
-	}
-});
+		
+            
+        }
+		
+		
+	
+        
+    }
+	
+// });
 
